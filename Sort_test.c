a0c00c2a46ac16d589dbf620770ef283a3092246
martinjobs23 <109801649+martinjobs23@users.noreply.github.com>
2022-08-18 15:34:33 +0800

    Update Sort_test.c

--- Sort_test.c
+++ Sort_test.c
@@ -4,38 +4,82 @@
 //
 //  Created by DFH on 2022/8/16.
 //
-
 #include <stdio.h>
-
-void scan(int a[]){
+void scan(int a[],int *pc){
     int i = 0;
+    int tmp;
     for (i = 0; i<100; i++) {
-        scanf("%d",&a[i]);
-        if (a[i] == '\n')
+        scanf("%d",&tmp);
+        a[i] = tmp;
+        (*pc)++;
+        char c = getchar();
+        if (c == '\n'){
             break;
+        }
     }
+}
+void sort(int a[],int num){
+    int i = 0;
+    int j = 0;
+    for (i = 0; i<num-1; i++) {
+        int flag = 0;
+        for (j = 0; j<num-1-i; j++) {
+            if(a[j]>a[j+1]){
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+                flag = 1;
+            }
+        }
+        if (flag == 0) {
+            break;
+        }
+    }
+}
+int sum(int a[],int num){
+    int i = 0;
+    int s = 0;
+    for (i = 0; i<num; i++) {
+        s = s +a[i];
+    }
+    return s;
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     //printf("Hello, World!\n");
-    //int a[21] = {280,158,222,309,361,413,252,440,308,288,194,129,387,163,334,165,160,161,164,162,468};
     int a[100];
-    scan(a);
+    int count = 0;
+    int *pc = &count;
+    scan(a,pc);
+    printf("共有%d个数字。排序后为：\n",count);
     int i = 0;
-    int j = 0;
-    for (i = 0; i<20; i++) {
-        for (j = 0; j<20-i; j++) {
-            if(a[j]>a[j+1]){
-                int tmp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = tmp;
-            }
-        }
-    }
-    for (i = 0; i<21; i++) {
+    sort(a,count);
+    int s = sum(a,count);
+    double avg = (double)s/count;
+    for (i = 0; i<count; i++) {
         printf("%d ",a[i]);
     }
-    
+    printf("\n");
+    printf("数列的和为%d，数列的平均数为%.2lf，最小值为%d，最大值为%d。\n",s,avg,a[0],a[count-1]);
+
     return 0;
 }
+//int main(){
+//    int num;
+//    int i=0;
+//    int arr[10];
+//   while(1){
+//        scanf("%d",&num);
+//        char c=getchar();
+//        arr[i++]=num;
+//        if(c=='\n'){
+//            break;
+//        }
+//   }
+//   /*for(int i=0;i<10;i++){
+//    printf("%d\n",arr[i]);
+//   }*/
+//  return 0;
+//}
+
